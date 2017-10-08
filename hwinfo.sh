1eb71c9e2a6af43b2f2c3a67482f22e4728057e2
puesyo <000cuenta0001@gmail.com>
2017-10-08 11:54:26 +0200

    k

---
+++ hwinfo.sh
@@ -0,0 +1,4 @@
+MHZ=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)
+TEMPE=$(cat /sys/class/thermal/thermal_zone0/temp)
+
+echo CPU Speed $(($MHZ/1000)) Mhz "|" CPU Temp $(($TEMPE/1000)) C
